b247bad77a09be2bd59537a6935586839c2c0646
pdolinic <71389319+pdolinic@users.noreply.github.com>
2023-04-18 02:15:57 +0200

    add espeak with movement reminder
    add espeak to remind standing up and moving after variable time

--- stopwatch.sh
+++ stopwatch.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # Authors: pdolinic, GPT-4
+# Info: Install the "espeak" package for the audio
+# Info: The script currently tells to stand up and move after 45 minutes
 
 stopwatch() {
     start_time=$(date +%s%N)
@@ -7,12 +9,21 @@ stopwatch() {
     echo "Stopwatch started at $current_time. Press ENTER to stop."
 
     enter="x"
+    beep_interval=$((45 * 60)) # 45 minutes in seconds
+    next_beep_time=$(( start_time + (beep_interval * 1000000000) ))
+
     while [ ! -z "${enter+x}" ]; do
         end_time=$(date +%s%N)
         elapsed_time=$(( (end_time - start_time) / 1000000 ))
         elapsed_minutes=$(( elapsed_time / 60000 ))
         elapsed_seconds=$(( (elapsed_time / 1000) % 60 ))
         elapsed_milliseconds=$(( elapsed_time % 1000 ))
+
+        if [ $end_time -ge $next_beep_time ]; then
+            espeak -v en+f3 "Stand up and move"
+            echo "Time to stand up and move!"
+            next_beep_time=$(( next_beep_time + (beep_interval * 1000000000) ))
+        fi
         
         printf "\rElapsed time: ${elapsed_minutes}m ${elapsed_seconds}.${elapsed_milliseconds}s"
         read -r -s -t 1 enter
